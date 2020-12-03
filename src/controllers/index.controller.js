ea04f0d36796dd76fd64cc6ca69437148c6ce63c
BlackPixl <javierdarioechavarria@gmail.com>
2020-12-03 13:17:50 -0500

    Updated index.controller logic

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -1,7 +1,126 @@
 const indexCtrl = {};
-const currentRoute = "/home/";
 const { exec } = require("child_process");
 
+const renderPage = (res, props) => {
+  exec('ls -p -A --group-directories-first', {cwd: props.cwd}, (err, stdout, stderr) => {
+    if (err) {
+      res.send("error, por favor recarga la pagina");
+    } else {
+      var files_folders = [];
+      elementos = stdout.split("\n");
+      elementos.pop();
+      var indexCount = 0;
+      elementos.forEach((element) => {
+        indexCount+=1;
+        if (element.substr(-1) == "/") {
+          files_folders.push({ type: "folder", name: element, folder: true, index: indexCount});
+        } else {
+          files_folders.push({ type: "file", name: element, folder: false, index: indexCount});
+        }
+      });
+      res.render("index", { files_folders, route: props.cwd });
+    }
+  });
+};
+
+const executeCommand = (req, res, props) => {
+  exec(props.command, {cwd: props.cwd}, (err, stdout, stderr) =>{
+    if (err){
+      res.send('Error, por favor vuelve a a pagina principal');
+    }else{
+      renderPage(res, props);
+    }
+  });
+};
+
+indexCtrl.renderIndexGet = (req, res) => {
+  props = {cwd: '/home/'}
+  res.cookie("route", props.cwd);
+  renderPage(res, props);
+};
+
+indexCtrl.renderIndexPost = (req, res) => {
+  var props = {};
+  var currentRoute = req.cookies.route;
+  action = req.body.action;
+  
+  switch (action) {
+    case 'enter':
+      currentRoute += req.body.folder;
+      res.cookie("route", currentRoute);
+      props = {cwd: currentRoute}
+      renderPage(res, props);
+      break;
+
+    case 'back':
+      var currentRoute = req.cookies.route;
+      const routeArray = currentRoute.split("/");
+      routeArray.pop();
+      routeArray.pop();
+      routeArray.push("");
+      currentRoute = routeArray.join("/");
+      res.cookie("route", currentRoute);
+      props = {
+        cwd: currentRoute,
+        command: 'ls -p -A --group-directories-first'
+      };
+      renderPage(res, props);
+      
+      break;
+
+    case 'delete':
+      console.log('paso por delete');
+      var objectType = req.body.typeObject;
+      if (objectType == 'folder'){
+        props = {
+          cwd: req.cookies.route,
+          command: 'rm -rf ' + req.body.nameObject
+        };
+      }else if (objectType == 'file'){
+        props = {
+          cwd: req.cookies.route,
+          command: 'rm -f ' + req.body.nameObject
+        };
+      }
+      executeCommand(req, res, props);
+      break;
+
+    case 'create_file':
+      props = {
+        cwd: req.cookies.route,
+        command: 'touch ' + req.body.nameObject
+      };
+      executeCommand(req, res, props);
+      break;
+
+    case 'create_folder':
+      props = {
+        cwd: req.cookies.route,
+        command: 'mkdir ' + req.body.nameObject
+      };
+      executeCommand(req, res, props);
+      break;
+
+    case 'rename':
+      var currentName = req.body.currentName;
+      props = {
+        cwd : req.cookies.route,
+        command : 'mv \''+currentName+'\' '+req.body.newName
+      };
+      executeCommand(req, res, props);
+      break;
+
+    default:
+      res.send("error, por favor recarga la pagina");
+      break;
+  }
+};
+
+module.exports = indexCtrl;
+
+/*OLD CODE */
+
+/*
 const renderPage = (res, stdout, currentRoute) => {
   var files_folders = [];
   elementos = stdout.split("\n");
@@ -17,92 +136,12 @@ const renderPage = (res, stdout, currentRoute) => {
   });
   res.render("index", { files_folders, route: currentRoute });
 };
+*/
 
-const executeCommand = (req, res, props) => {
-  exec(props.command, {cwd: props.cwd}, (err, stdout, stderr) =>{
-    if (err){
-      res.send('Error, por favor vuelve a a pagina principal');
-    }else{
-      exec(
-        "ls -p -A --group-directories-first", { cwd: props.cwd }, (err, stdout, stderr) => {
-          if (err) {
-            res.send("error, por favor recarga la pagina");
-          } else {
-            renderPage(res, stdout, props.cwd);
-          }
-        }
-      );
-    }
-  });
-};
 
-indexCtrl.renderIndexGet = (req, res) => {
-  res.cookie("route", currentRoute);
-  exec(
-    "ls -p -A --group-directories-first",
-    { cwd: currentRoute },
-    (err, stdout, stderr) => {
-      if (err) {
-        // console.log(stderr);
-        // console.log("hola");
-        // console.log(err);
-        res.send("error, por favor recarga la pagina");
-      } else {
-        renderPage(res, stdout, currentRoute);
-      }
-    }
-  );
-};
 
-indexCtrl.renderIndexPost = (req, res) => {
-  var currentRoute = req.cookies.route;
-  action = req.body.action;
-  console.log(action);
-  console.log(currentRoute);
-  console.log(req.body);
 
-  switch (action) {
-    case "enter":
-      currentRoute += req.body.folder;
-      res.cookie("route", currentRoute);
-      exec(
-        "ls -p -A --group-directories-first",
-        { cwd: currentRoute },
-        (err, stdout, stderr) => {
-          if (err) {
-            console.log(err);
-            console.log(currentRoute);
-            res.send("error, por favor recarga la pagina");
-          } else {
-            renderPage(res, stdout, currentRoute);
-          }
-        }
-      );
-      break;
-
-    case "back":
-      var currentRoute = req.cookies.route;
-      console.log(currentRoute);
-      const routeArray = currentRoute.split("/");
-      routeArray.pop();
-      routeArray.pop();
-      routeArray.push("");
-      currentRoute = routeArray.join("/");
-      res.cookie("route", currentRoute);
-      exec(
-        "ls -p -A --group-directories-first",
-        { cwd: currentRoute },
-        (err, stdout, stderr) => {
-          var files_folders = [];
-          if (err) {
-            res.send("error, por favor recarga la pagina");
-          } else {
-            renderPage(res, stdout, currentRoute);
-          }
-        }
-      );
-      break;
-    
+  /*  
     case "delete":
       var currentRoute = req.cookies.route;
       var nameObject = req.body.nameObject;
@@ -164,7 +203,10 @@ indexCtrl.renderIndexPost = (req, res) => {
           break;
       }
       break;
+*/
 
+
+/* 
     case "create_file":
       var nameObject = req.body.nameObject;
       console.log(req.body);
@@ -189,9 +231,10 @@ indexCtrl.renderIndexPost = (req, res) => {
           }
         }
       );
-
       break;
+*/
 
+/*
     case "create_folder":
       var nameObject = req.body.nameObject;
       console.log(req.body);
@@ -218,21 +261,17 @@ indexCtrl.renderIndexPost = (req, res) => {
       );
 
       break;
+*/
 
-    case 'rename':
-      var currentName = req.body.currentName;
-      console.log(req.body);
-      const command = 'mv \''+currentName+'\' '+req.body.newName;
-      props = {
-        cwd : req.cookies.route,
-        command : command};
-      executeCommand(req, res, props);
-      break;
-
-    default:
-      res.send("error, por favor recarga la pagina");
-      break;
-  }
-};
-
-module.exports = indexCtrl;
+/*exec(
+        "ls -p -A --group-directories-first",
+        { cwd: currentRoute },
+        (err, stdout, stderr) => {
+          var files_folders = [];
+          if (err) {
+            res.send("error, por favor recarga la pagina");
+          } else {
+            renderPage(res, stdout, currentRoute);
+          }
+        }
+      );*/
